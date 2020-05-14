73fa7d1aa3ed7600bf721d4f892299943a69653a
Hammel <b0916865446@gmail.com.tw>
2020-05-14 13:09:34 +0800

    end page

--- app/src/main/java/com/example/tool/MainActivity.java
+++ app/src/main/java/com/example/tool/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.tool;
 
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Path;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -38,13 +42,17 @@ public class MainActivity extends AppCompatActivity {
     int number[]=imgId;
     int point2[] = point;
     int countri=1;
+
+    //nt[] use_card = new int [5];
+    int[] use_card = {0,0,0,0,0};
+
+
     poker countpoint = new poker();
 
     private Button puls;
     private ImageView pokerback,you_poker;
     private ImageView[] rightp = new ImageView[5];
-    private TextView nn,nowpoint,inputpoint;
-
+    private TextView nn,nowpoint,inputpoint,yy;
 
     android.animation.ObjectAnimator ObjectAnimator;
 
@@ -76,21 +84,26 @@ public class MainActivity extends AppCompatActivity {
             int count = number.length - use;
             int p = (int) (Math.random() * count);
             you_poker.setImageResource(number[p]);
-
             use++;
 
             int list = count-1;
             nn.setText("剩餘牌數："+ list);
 
+            use_card[0] = point2[p];
+
             float a = countpoint.Score(point2[p]);
             nowpoint.setText("目前點數： "+String.valueOf(a));
-
+/*
             android.graphics.Path path = new Path();
             path.arcTo(500f,0f,1000f,800f,180f,140f,true);
             ObjectAnimator= android.animation.ObjectAnimator.ofFloat(you_poker,View.X,View.Y,path);
             ObjectAnimator.setDuration(1000);
             ObjectAnimator.start();
-
+*/
+            Animation am = new TranslateAnimation(0,759,0,-259);
+            am.setDuration(5000);
+            you_poker.setAnimation(am);
+            am.startNow();
             Handler handler = new Handler();
             rightp[0].setImageResource(number[p]);
 
@@ -101,14 +114,10 @@ public class MainActivity extends AppCompatActivity {
                 point2[i] = point2[i+1];
                 number[i] = number[i + 1];
             }
-
-
-
         }
 
 
     }
-
 
     private final Button.OnClickListener
             pulsLin = new Button.OnClickListener() {
@@ -121,17 +130,29 @@ public class MainActivity extends AppCompatActivity {
                         you_poker.setImageResource(number[p]);
                         use++;
                         rightp[countri].setImageResource(number[p]);
+                        use_card[countri] = point2[p];
+
                         countri++;
                         int list = count-1;
                         nn.setText("剩餘牌數："+ list);
 
                         float a = countpoint.Score(point2[p]);
+
                         if(a<=10.5)
                             nowpoint.setText("目前點數： "+ String.valueOf(a));
                         else {
-                            nowpoint.setText("爆點啦!");
-                            puls.setEnabled(false);
-                            inputpoint.setText("你目前點數: " + a);
+                            finish();
+                            Intent intent = new Intent();
+                            intent.setClass(MainActivity.this, end_page.class);
+
+
+                            Bundle bundle = new Bundle();
+                            bundle.putFloat("nowpoint",a);
+                            bundle.putIntArray("use_card",use_card);
+
+                            intent.putExtras(bundle);
+                            startActivity(intent);
+
                         }
 
 
